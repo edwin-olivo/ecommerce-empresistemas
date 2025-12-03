11129ac2c0f2c4a2145a1e7c4fec88bcfdcd3b19
Edwin Olivo <46498127+edwin-olivo@users.noreply.github.com>
2025-12-03 09:44:26 -0700

    feat: update wishlist method to redirect to wishlist route

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -18,6 +18,6 @@ class DashboardController extends Controller
 
     function wishlist()
     {
-        return inertia('dashboard/wishlist');
+        return redirect()->route('wishlist');
     }
 }
