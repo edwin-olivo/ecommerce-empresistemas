2cab1910a26d544a408f8d7256aed067e547f492
Edwin Olivo <46498127+edwin-olivo@users.noreply.github.com>
2025-12-03 09:44:22 -0700

    feat: update dashboard routes to use WhishlistController for wishlist management

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -2,11 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\WhishlistController;
 
 Route::middleware('auth')->group(function () {
     // Rutas para el perfil del usuario (ordenes, historial, direcciones, etc.)
     Route::redirect('/dashboard', '/dashboard/orders');
     Route::get('/dashboard/orders', [DashboardController::class, 'orders'])->name('dashboard.orders');
     Route::get('/dashboard/addresses', [DashboardController::class, 'addresses'])->name('dashboard.addresses');
-    Route::get('/dashboard/wishlist', [DashboardController::class, 'wishlist'])->name('dashboard.wishlist');
+
+    Route::resource('/dashboard/wishlist', WhishlistController::class)->names([
+        'index' => 'wishlist',
+        'store' => 'wishlist.store',
+        'show' => 'wishlist.show',
+        'update' => 'wishlist.update',
+        'destroy' => 'wishlist.destroy',
+    ]);
 });
