7f83f656116937a37f20b4693aba38fca878235a
Edwin Olivo <46498127+edwin-olivo@users.noreply.github.com>
2025-12-08 10:13:53 -0700

    fix: adjust max width of content area in DashboardLayout for better responsiveness

--- resources/js/layouts/common/dashboard-layout.tsx
+++ resources/js/layouts/common/dashboard-layout.tsx
@@ -59,7 +59,7 @@ export default function DashboardLayout({ children }: PropsWithChildren) {
 
                 <Separator className="my-6 lg:hidden" />
 
-                <div className="flex-1 md:max-w-2xl lg:max-w-4xl">
+                <div className="flex-1 md:max-w-full lg:max-w-4xl">
                     <section className="space-y-12">{children}</section>
                 </div>
             </div>
